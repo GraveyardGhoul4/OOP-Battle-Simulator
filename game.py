740737bed2beada06e1500b325509484acfcb8ad
GraveyardGhoul4 <aidensals@outlook.com>
2026-09-11 09:29:37 -0500

    Added New Goblin

--- game.py
+++ game.py
@@ -13,6 +13,11 @@ def main():
     goblin = Goblin("Hayes Harris")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    newGoblin = Goblin("Scribble")
+    
+    print(f"{newGoblin.name} enters the arena with {newGoblin.health} health.")
+
     print("But no hero has answered the call... yet.")
 
 
